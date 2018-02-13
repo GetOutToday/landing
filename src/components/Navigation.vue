ce14b0e730f59296a3922f760ef30ad5a958b573
Aksels Ledins <aksels.ledins@gmail.com>
2018-02-13 18:37:22 +0100

    (c) Navigation 0.1

---
+++ src/components/Navigation.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container-lg">
+    <div class="nav-container">
+      <div class="logo-container">
+        <a class="nav-item">
+          GetOut.today
+        </a>
+      </div>
+      <a class="nav-item">Features</a>
+      <a class="nav-item">Demo</a>
+      <div class="login-container">
+        <a class="nav-item">Try it free</a>
+        <a class="nav-item">Sign in</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+};
+</script>
+
+<style scoped>
+
+.nav-item {
+  padding: 0 25px;
+  display: inline-block;
+  height: 50px;
+  white-space: nowrap;
+  user-select: none;
+  font-size: 17px;
+  line-height: 50px;
+  margin: 0;
+}
+
+.nav-container, .login-container {
+  position: relative;
+  display: flex;
+}
+
+.nav-container {
+  justify-content: center;
+}
+
+.login-container {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+
+.logo-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+
+import Navigation from '@/components/Navigation';
+
 import App from './App';
 import router from './router';
 
+
 Vue.config.productionTip = false;
+
+Vue.component('navigation', Navigation);
 
 /* eslint-disable no-new */
 new Vue({
